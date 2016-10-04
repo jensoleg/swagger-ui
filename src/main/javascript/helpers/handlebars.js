5e0062bd12fd724e5824ade58ef8e4e47ccc214f
Luke Epp <lucasfepp@gmail.com>
2016-10-04 14:36:12 -0700

    Remove show samples button when displaying collapse samples button

--- src/main/javascript/helpers/handlebars.js
+++ src/main/javascript/helpers/handlebars.js
@@ -13,3 +13,10 @@ Handlebars.registerHelper('times', function(n, html) {
   }
   return sum;
 });
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if (a == b) {
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
